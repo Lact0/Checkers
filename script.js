04d1ee3764b53481a00b537b73943d072497aa34
Peter Perry <99445714+Lact0@users.noreply.github.com>
2022-09-21 13:26:23 +0000

    stuff

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 window.onresize = changeWindow;
 let mainBoard = new Board();
-let gameStarted = false;
 let overlay;
 let text;
-let playerTurn;
-let canClick = false;
 
 function load() {
   canvas = document.querySelector('.canvas');
@@ -15,79 +12,24 @@ function load() {
   canvas.height = height;
   document.onkeydown = keyPress;
   document.onmousemove = updateMousePos;
-  //runFrame();
+  overlay.style.display = "none";
+  text.style.display = 'block';
+  mainBoard.draw(0, 0, width, height);
 }
 
 function startGame(n) {
-  playerTurn = n;
-  canClick = !(n == 2);
   overlay.style.display = "none";
   text.style.display = 'block';
-  gameStarted = true;
-  mainBoard = new Board();
-  text.innerHTML = 'Player Turn';
-  if(n == 2) {
-    text.innerHTML = 'Ai Turn';
-    aiMove();
-  }
-  redraw();
 }
 
 function finishGame() {
-  canClick = false;
   overlay.style.display = 'block';
   text.style.display = 'none';
-  gameStarted = false;
-  //ctx.clearRect(0, 0, width, height);
 }
 
 function redraw() {
   ctx.clearRect(0, 0, width, height);
   mainBoard.draw(0, 0, width, height);
-}
-
-function aiDecide(board, maxPlayer, a = Number.MIN_VALUE, b = Number.MAX_VALUE, depth = 10) {
-  const moves = board.getMoves();
-  if(board.winner != 0 || moves.length == 0 || depth == 0) {
-    return [false, board.getVal()];
-  }
-  let ext;
-  let bestMove;
-  for(let i = 0; i < moves.length; i++) {
-    const move = moves[i];
-    const newBoard = board.makeMove(move);
-    const outcome = aiDecide(newBoard, !maxPlayer, a, b, depth - 1);
-    if(ext == null) {
-      bestMove = move;
-      ext = outcome[1];
-    }
-    if(maxPlayer) {
-      if(outcome[1] > ext) {
-        bestMove = move;
-        ext = outcome[1];
-      }
-      if(ext >= b) {
-        break;
-      }
-      a = max(a, ext);
-    } else {
-      if(outcome[1] < ext) {
-        bestMove = move;
-        ext = outcome[1];
-      }
-      if(ext <= a) {
-        break;
-      }
-      b = min(b, ext);
-    }
-  }
-  //TEMPORARY
-  //IF THE BEST PLAY STILL RESULTS IN LOSS,
-  //CHOOSE RANDOM MOVE, NOT FIRST
-  if((maxPlayer && ext == -10000) || (!maxPlayer && ext == 10000)) {
-    bestMove = moves[rand(0, moves.length - 1)];
-  }
-  return [bestMove, ext];
 }
 
 function changeWindow() {
@@ -97,52 +39,25 @@ function changeWindow() {
 }
 
 function keyPress(key) {
-  if(key.keyCode != 32) {
+  if(key.keyCode == 32) {
     return;
   }
 }
 
 function leftClick() {
-  if(!canClick) {
-    return;
-  }
-  const x = event.clientX;
-  const y = event.clientY;
-  const xSpace = width / 7;
-  const ySpace = height / 6;
-  const space = min(xSpace, ySpace);
-  const xStart = (width - (space * 6)) / 2;
-  const mouseX = Math.floor((mousePos[0] - xStart - (space * .5)) / space) + 1;
-  if(!gameStarted || (playerTurn != mainBoard.toMove && playerTurn != 3)) {
-    return;
-  }
-  if(mainBoard.arr[mouseX][5] == 0) {
-    mainBoard = mainBoard.makeMove(mouseX);
-    redraw();
-    if(mainBoard.winner != 0 || mainBoard.movesLeft == 0) {
-      canClick = false;
-      showWinner();
-      setTimeout(finishGame, 1000);
-      return;
-    }
-    if(playerTurn != 3) {
-      canClick = false;
-      text.innerHTML = 'Ai Turn';
-      setTimeout(aiMove, 1000);
-    }
-  }
-  redraw();
+  
 }
 
 function aiMove() {
   let move;
   let player = playerTurn == 2;
+  console.log(player)
   move = aiDecide(mainBoard, player)[0];
   mainBoard = mainBoard.makeMove(move);
   redraw();
   if(mainBoard.winner != 0 || mainBoard.movesLeft == 0) {
     showWinner();
-    setTimeout(finishGame, 3000);
+    setTimeout(finishGame, 5000);
     return;
   }
   text.innerHTML = 'Player Turn';
@@ -152,32 +67,4 @@ function aiMove() {
 function updateMousePos() {
   mousePos[0] = event.clientX;
   mousePos[1] = event.clientY;
-  if(gameStarted) {
-    redraw();
-  }
-}
-
-function showWinner() {
-  let t;
-  switch(playerTurn) {
-    case 1:
-      if(mainBoard.winner == 1) {
-        t = 'You Win!';
-      } else {
-        t = 'Ai Wins!';
-      }
-      break;
-    case 2:
-      if(mainBoard.winner == 1) {
-        t = 'Ai Wins!';
-      } else {
-        t = 'You Win!';
-      }
-      break;
-    case 3:
-      t = 'Player ' + mainBoard.winner + ' Wins!'; 
-      break;
-  }
-
-  text.innerHTML = t;
 }
--- setup.js
+++ setup.js
@@ -91,186 +91,92 @@ function intersects(p11, p12, p21, p22) {
   return false;
 }
 
-//Classes
-class Vector {
-  constructor(x = 0, y = 0, x0 = 0, y0 = 0) {
-    this.x = x - x0;
-    this.y = y - y0;
-    this.getMag();
-  }
-
-  getMag() {
-    this.mag = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-  }
-
-  normalize() {
-    if(this.mag == 0) {
-      return;
-    }
-    this.x /= this.mag;
-    this.y /= this.mag;
-    this.getMag();
-  }
-
-  setMag(mag) {
-    this.normalize();
-    this.x *= mag;
-    this.y *= mag;
-    this.mag = mag;
-  }
-
-  limit(mag) {
-    this.getMag();
-    if(this.mag > mag) {
-      this.setMag(mag);
-    }
-  }
-
-  copy() {
-    return new Vector(this.x, this.y);
-  }
-
-  add(vector) {
-    this.x += vector.x;
-    this.y += vector.y;
-    this.getMag();
-  }
-
-  sub(vector) {
-    this.x -= vector.x;
-    this.y -= vector.y;
-    this.getMag();
-  }
-
-  distTo(vector) {
-    return Math.sqrt(Math.pow(this.x - vector.x, 2) + Math.pow(this.y - vector.y, 2));
-  }
+function inRange(n) {
+  return n >= 0 && n < 8;
 }
 
+//Classes
 class Board {
-  constructor() {
-    this.arr = [[0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],];
-    this.toMove = 1;
-    this.winner = 0;
-    this.movesLeft = 42;
+  constructor(params) {
+    this.arr = params.arr || [[2, 0, 2, 0, 0, 0, 1, 0],
+                [0, 2, 0, 0, 0, 1, 0, 1],
+                [2, 0, 2, 0, 0, 0, 1, 0],
+                [0, 2, 0, 0, 0, 1, 0, 1],
+                [2, 0, 2, 0, 0, 0, 1, 0],
+                [0, 2, 0, 0, 0, 1, 0, 1],
+                [2, 0, 2, 0, 0, 0, 1, 0],
+                [0, 2, 0, 0, 0, 1, 0, 1]]
+    this.toGo = params.toGo || 1;
+    this.black = params.black || 12;
+    this.white = params.white || 12;
+    this.getWinner();
+    this.getMoves();
   }
 
-  draw(x = 0, y = 0, w = width, h = height) {
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'white';
-    const xSpace = w / 7;
-    const ySpace = h / 6;
-    const space = min(xSpace, ySpace);
-    const xStart = (w - (space * 6)) / 2;
-    const yStart = (h - (space * 5)) / 2;
-    const r = space / 4;
-    for(let i = 0; i < 7; i++) {
-      for(let j = 0; j < 6; j++) {
-        ctx.beginPath();
-        ctx.arc(xStart + i * space, yStart + j * space, r, 0, Math.PI * 2);
-        ctx.stroke();
-        switch(this.arr[i][5 - j]) {
-          case 1:
-            ctx.fillStyle = 'blue';
-            ctx.fill();
-            break;
-          case 2:
-            ctx.fillStyle = 'red';
-            ctx.fill();
-            break;
-        }
-      }
-    }
-    const mouseX = Math.floor((mousePos[0] - xStart - (space * .5)) / space) + 1;
-    if(mouseX >= 0 && mouseX < 7) {
-      ctx.fillStyle = 'rgba(100, 100, 100, .5)';
-      ctx.beginPath();
-      ctx.arc(xStart + space * mouseX, yStart, r * 2, 0, Math.PI * 2);
-      ctx.fill();
+  getWinner() {
+    if(this.white == 0) {
+      this.winner = 2;
+    } else if(this.black == 0) {
+      this.winner = 1;
+    } else {
+      this.winner = 0;
     }
   }
 
   getMoves() {
-    let moves = [];
-    for(let i = 0; i < 7; i++) {
-      if(this.arr[i][5] == 0) {
-        moves.push(i);
-      }
-    }
-    return moves;
-  }
-  
-  makeMove(row) {
-    const newBoard = this.copy();
-    
-    if(this.arr[row][5] != 0) {
-      return false;
-    }
-    let pos = 5;
-    while(this.arr[row][pos] == 0 && pos >= 0) {
-      pos -= 1;
-    }
-    pos++;
-    newBoard.arr[row][pos] = this.toMove;
-    newBoard.toMove = (this.toMove % 2) + 1;
-
-    //Check for Win
-    const directions = [[1, 0], [0, 1], [1, 1], [1, -1]];
-    for(let dir of directions) {
-      let n = 1;
-      let p = {x: row, y: pos};
-      for(let j = 0; j < 2; j++) {
-        let mult = j * 2 - 1;
-        let cont = true;
-        let i = 1;
-        while(cont) {
-          let color;
-          try {
-            color = this.arr[p.x + dir[0] * i * mult][p.y + dir[1] * i * mult];
-          } catch {
-            break;
-          }
-          if(color != this.toMove) {
-            break;
-          }
-          n++;
-          i++;
+    this.moves = [];
+    //Move is an object
+    //{start: pos, end: pos, kills: [pos,pos]}
+    for(let i = 0; i < 8; i++) {
+      for(let j = 0; j < 8; j++) {
+        if(this.arr[i][j] != this.toGo) {
+          continue;
+        }
+        let dir = (this.toGo == 2? 1: -1);
+        if(inRange(i + dir) && inRange(j + 1) && this.arr[i + dir, j + 1] == 0) {
+          this.moves.push({start: [i, j], end: [i + dir, j + 1]})
         }
       }
-      if(n >= 4) {
-        newBoard.winner = this.toMove;
+    }
+  }
+
+  draw(x, y, w, h) {
+    const unit = Math.floor(min(w, h) / 9);
+    const xStart = Math.floor((w - (unit * 8)) / 2);
+    const yStart = Math.floor((h - (unit * 8)) / 2);
+    ctx.fillStyle = 'rgba(255, 255, 255, .2)';
+    ctx.strokeStyle = 'white';
+    for(let i = 0; i < 8; i++) {
+      for(let j = 0; j < 8; j++) {
+        ctx.fillStyle = 'rgba(255, 255, 255, .2)';
+        const xp = xStart + unit * i;
+        const yp = yStart + unit * j;
+        if((i + j) % 2 == 0) {
+          ctx.fillRect(xp, yp, unit, unit);
+        }
+        ctx.strokeRect(xp, yp, unit, unit);
+        if(this.arr[i][j] != 0) {
+          ctx.fillStyle = 'rgba(0, 0, 48, 1)';
+          if(this.arr[i][j] == 1) {
+            ctx.fillStyle = 'rgba(51, 51, 99, 1)';
+          }
+          ctx.beginPath();
+          const r = unit / 2;
+          ctx.arc(xp + r, yp + r, r * .8, 0, Math.PI * 2);
+          ctx.fill();
+          ctx.stroke();
+        }
       }
     }
-    newBoard.movesLeft--;
-    return newBoard;
   }
 
   copy() {
-    const newBoard = new Board();
-    for(let i = 0; i < this.arr.length; i++) {
-      for(let j = 0; j < this.arr[i].length; j++) {
-        newBoard.arr[i][j] = this.arr[i][j];
-      }
-    }
-    newBoard.toMove = this.toMove;
-    newBoard.movesLeft = this.movesLeft;
-    return newBoard;
-  }
-
-  getVal() {
-    switch(this.winner) {
-      case 1:
-        return 10000;
-      case 2:
-        return -10000;
-    }
-    let ret = 0;
-    return ret;
+    const child = new Board();
+    child.arr = JSON.parse(JSON.stringify(this.arr));
+    child.toGo = this.toGo;
+    child.black = this.black;
+    child.white = this.white;
+    child.getWinner();
+    child.getMoves();
   }
 }
